b0e12448f41469b83acec9d4e2e9c9f72b5b3946
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-28 09:52:57 -0400

    On first click X appends but on second click no O

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const gameBoard = (function () {
+  const gameBox = document.querySelectorAll(".game-box");
   const bordBoxes = document.querySelectorAll("[data-index]");
-  return { bordBoxes };
+
+  gameBox.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      console.log(event);
+
+      item.innerHTML = "X";
+    });
+  });
+
+  return { bordBoxes, gameBox };
 })();
 
 function player(name, marker) {
-  return { name, marker };
+  let score = 0;
+  const scoreLevel = () => {
+    score++;
+    return score;
+  };
+  return { name, marker, scoreLevel, score };
+}
+
+function winner() {
+  const { name, marker, score } = player(name, marker, score);
+  if (player.score === 3) {
+    return `${player.name} is the Winner`;
+  }
 }
