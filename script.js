972f7961abe83149575939f27ba53e6ddaff37f6
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-30 11:21:33 -0400

    TicTacToe basic game logic

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const gameBoard = (function () {
   const gameBox = document.querySelectorAll(".game-box");
   const bordBoxes = document.querySelectorAll("[data-index]");
-
+  const resetGame = document.querySelector(".rest-game");
+  let clickCount = 0;
   gameBox.forEach((item) => {
     item.addEventListener("click", (event) => {
-      console.log(event);
+      clickCount++;
 
-      item.innerHTML = "X";
+      if (clickCount % 2 !== 0) {
+        item.innerHTML = "X";
+      } else {
+        item.innerHTML = "O";
+      }
+    });
+    resetGame.addEventListener("click", (event) => {
+      item.innerHTML = " ";
     });
   });
 
@@ -22,7 +30,7 @@ function player(name, marker) {
   return { name, marker, scoreLevel, score };
 }
 
-function winner() {
+function winnerOfGame() {
   const { name, marker, score } = player(name, marker, score);
   if (player.score === 3) {
     return `${player.name} is the Winner`;
